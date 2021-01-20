c00d5e6e6c77c0a0d8302ad496f1e052a14be3ba
DESKTOP-KII3PPC\mikae <mtovaska@jyu.fi>
2021-01-20 21:32:05 +0200

    use time as name for entity in database
    not sure if it is a good idea or not, but no overwrites at least

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 import os
 from google.cloud import datastore
-
+import time
 
 app = Flask(__name__)
 
@@ -9,18 +9,17 @@ datastore_client = datastore.Client('agile-team-299406')
 
 @app.route('/')
 def home():
-
-
     return render_template('mymap.html')
 
 @app.route('/save', methods=['POST'])
 def save_to_db():
     data = request.data.decode()
     kind = 'HiveLocation'
-    name = 'beemap1'
-    task_key = datastore_client.key('HiveLocation')
+    name = time.strftime('%Y-%m-%d-%H:%M:%S')
+    task_key = datastore_client.key(kind, name)
     task = datastore.Entity(key=task_key)
     task['location'] = data
+    task['bonus'] = 'something'
     
     datastore_client.put(task)
     print (data)
@@ -28,17 +27,14 @@ def save_to_db():
 
 @app.route('/delete', methods=['DELETE'])
 def delete_from_db():
-    data = request.data
-    print (data)
-    return (data)
+    pass
 
 @app.route('/update', methods=['GET'])
 def load_db():
     query = datastore_client.query(kind='HiveLocation')
     query.order = ['location']
-    a = list(query.fetch())
-
-
+    data = list(query.fetch())
+    return data
 
 if __name__ == '__main__':
     host = os.getenv("HOST", "127.0.0.1")
