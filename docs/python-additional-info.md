03e5c3e8e9ec3b2388b568ea8d10db5e678cd6df
Teemu Autto <teemu@rautakuu.org>
2021-01-22 10:01:17 +0200

    Python lecture changes. Fetch items from datastore and show them in map.

---
+++ docs/python-additional-info.md
@@ -0,0 +1,11 @@
+Jinja2:
+Template system https://jinja2docs.readthedocs.io/en/stable/
+
+Flask:
+Microframework that takes care of changing code into a web page https://flask.palletsprojects.com/en/1.1.x/
+
+Pip:
+Takes care of fetching python packages from internet and installing them. https://pypi.org/project/pip/
+
+Decorators:
+Envelopes function around another function.
--- main.py
+++ main.py
@@ -2,26 +2,39 @@ from flask import Flask, render_template, request, jsonify
 import os
 from google.cloud import datastore
 import time
+import uuid
 
 app = Flask(__name__)
 
-datastore_client = datastore.Client('agile-team-299406')
+datastore_client = datastore.Client('continousdegrader')
 
 @app.route('/')
 def home():
-    return render_template('mymap.html')
+
+    locations = []
+    for latlng in datastore_client.query(kind='HiveLocation').fetch():
+        locations.append({
+            "lat": latlng['location'].latitude,
+            "lon": latlng['location'].longitude
+        })
+
+    return render_template('mymap.html', hive_locations=locations)
+
 
 @app.route('/save', methods=['POST'])
 def save_to_db():
     data = request.data.decode()
+
+
     kind = 'HiveLocation'
-    name = time.strftime('%Y-%m-%d-%H:%M:%S')
+    name = uuid.uuid5("farts", "foo")
     task_key = datastore_client.key(kind, name)
     task = datastore.Entity(key=task_key)
     task['location'] = data
     task['bonus'] = 'something'
-    
+
     datastore_client.put(task)
+
 
 @app.route('/delete', methods=['DELETE'])
 def delete_from_db():
@@ -33,6 +46,8 @@ def load_db():
     query.order = ['location']
     data = list(query.fetch())
     print(data)
+
+
 
 if __name__ == '__main__':
     host = os.getenv("HOST", "127.0.0.1")
