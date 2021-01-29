db809c888b8cd06c37fdf5c4f7257ab57d9483e0
Teemu Autto <teemu@rautakuu.org>
2021-01-29 16:09:16 +0200

    Test for finnish translations.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,19 @@
+import os
+import sys
+import pytest
+# Add parent directory as module search path
+sys.path.append(os.path.dirname(__file__) + "/..")
+
+
+# Prepare flask for testing
+from main import app as _app # noqa
+_app.config['TESTING'] = True
+
+@pytest.fixture
+def app():
+    return _app
+
+
 def pytest_configure(config):
     # Declare custom marker for staging tests
     config.addinivalue_line(
