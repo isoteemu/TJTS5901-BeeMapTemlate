73b581634895a18ffe8cf24693b57beb4f47df3c
Teemu Autto <teemu@rautakuu.org>
2021-02-04 13:20:29 +0200

    Added demonstrated flask routing test.

--- tests/test_app.py
+++ tests/test_app.py
@@ -12,3 +12,12 @@ def test_finnish_translations(app):
         with force_locale('fi_FI'):
             assert _("Hello World") == "Hei Maailma", "Message is not translated"
 
+
+def test_fetch_mainpage(app):
+    """ Fetch mainpage and check that it has <title>
+    """
+
+    with app.test_client() as client:
+        page = client.get('/')
+        assert page, "Did not get anything"
+        assert b"<title>" in page.data, "Page didn't have TITLE"
